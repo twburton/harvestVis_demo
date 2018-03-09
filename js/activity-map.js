81f86cba252eb5762a84570ecbdc90892bf51b73
Nathan Zimpfer <thoufer@gmail.com>
2018-03-09 12:03:47 -0500

    Update Visualization 1
    * Shrink map to be more navigational in nature,
    * add flyway boundaries along with text labels to display flyway name & season length in days.
    * Add placeholder labels for additional metrics
    
    The underlying base map changed, as the original one used a weird
    projection that I was unable to match to get boundary lines to overlay
    correctly.

--- js/activity-map.js
+++ js/activity-map.js
@@ -1,13 +1,59 @@
 function drawMap() {
   var svg = d3.select("#activity-map");
-  var path = d3.geoPath();
   var stateInfo = d3.map();
+  var projection = d3.geoAlbersUsa()
+    .scale([1050]);
+  var path = d3.geoPath().projection(projection);
 
   resize();
   d3.select(window).on("resize", resize);
 
+  // Add text labels
+  var textLabels = svg.append("g");
+
+  textLabels.append("text")
+    .attr("x", 165)
+    .attr("y", 15)
+    .text("Pacific Flyway");
+
+  textLabels.append("text")
+    .attr("x", 185)
+    .attr("y", 50)
+    .text("107 Days");
+
+  textLabels.append("text")
+    .attr("x", 330)
+    .attr("y", 15)
+    .text("Central Flyway");
+
+    textLabels.append("text")
+      .attr("x", 350)
+      .attr("y", 50)
+      .text("74 Days");
+
+  textLabels.append("text")
+    .attr("x", 500)
+    .attr("y", 15)
+    .text("Mississippi Flyway");
+
+  textLabels.append("text")
+    .attr("x", 540)
+    .attr("y", 50)
+    .text("60 Days");
+
+  textLabels.append("text")
+    .attr("x", 700)
+    .attr("y", 15)
+    .text("Atlantic Flyway");
+
+  textLabels.append("text")
+    .attr("x", 720)
+    .attr("y", 50)
+    .text("60 Days");
+
   d3.queue()
-     .defer(d3.json, "https://d3js.org/us-10m.v1.json")
+     .defer(d3.json, "js/us-states.json") // from: http://bl.ocks.org/mbostock/raw/4090846/us.json
+     .defer(d3.json, "js/FlywayBoundaryLine.geo.json")
      .defer(d3.csv, "js/States.csv", function(d){ stateInfo.set(d.id, {'name':d.name,
                                                                     'ab':d.abbrev,
                                                                     'days':d.days,
@@ -17,17 +63,18 @@ function drawMap() {
      .await(ready);
 
 
-  function ready(error, us) {
+  function ready(error, us, FlywayBoundaryLine) {
      if (error) throw error;
 
      svg.append("g")
          .attr("class", "states")
+         .attr("transform", "translate(0, 50)")
        .selectAll("path")
        .data(topojson.feature(us, us.objects.states).features)
        .enter().append("path")
          .attr("d", path)
          .attr("fill", function(d){
-           if(d.id == 15){
+           if(d.id === 15){
              return "gray";
            }
          })
@@ -44,18 +91,33 @@ function drawMap() {
               // Select current item
               d3.select(this).classed("selected", true);
             }
-           });
+          });
 
-      // Change Hawaii fill color.
+
      svg.append("path")
          .attr("class", "state-borders")
+         .attr("transform", "translate(0, 50)")
          .attr("d", path(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; })));
-       };
 
-       function resize() {
-         width = window.innerWidth, height = window.innerHeight;
-         svg.attr("width", width).attr("height", height)
-         svg.size([width, height]);
-       };
+     svg.append("g")
+       .attr("class", "flyway")
+       .attr("transform", "translate(0, 50)")
+       .selectAll("path")
+       .data(FlywayBoundaryLine.features)
+       .enter().append("path")
+         .attr("d", path)
+         .attr("fill", "none")
+         .attr("stroke", '#666666')
+         .attr("stroke-width", "3px");
+   };
+
+
+
+   function resize() {
+     width = $('#map-container').width(), height = $('#map-container').height();
+     svg.attr("width", width).attr("height", height)
+     svg.size([width, height]);
+     console.log(width, height)
+   };
 }
 drawMap();
