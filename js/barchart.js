a5d713975581056e921d446d46545c9fdb66cd08
Nathan Zimpfer <thoufer@gmail.com>
2018-04-30 09:49:40 -0400

    Make visualization 2 more compact
    * Reduce the size of the map, and change the location
    * shrink the size of the barchart a tiny bit.. more through, shrinking
      of the svg itself.

--- js/barchart.js
+++ js/barchart.js
@@ -4,10 +4,10 @@ var months = [{month:'September', week:35},
               {month:'December',  week:50},
               {month:'January',   week:55}];
 
-var width = 800, height = 600
+var width = 700, height = 650
     margin = {top: 10, right: 0, bottom: 50, left: 50};
     innerWidth = width - margin.left - margin.right,
-    innerHeight = height - margin.top - margin.bottom - 150,
+    innerHeight = height - margin.top - margin.bottom,
     xScale = d3.scaleBand()
                 .rangeRound([0, innerWidth])
                 .padding(0.2),
@@ -25,7 +25,7 @@ var svg = d3.select("#barchart")
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 d3.json("js/us-states.json", function (error, us){
-  var projection = d3.geoAlbersUsa().scale(500).translate([500,100]);
+  var projection = d3.geoAlbersUsa().scale(400).translate([550,75]);
   var path = d3.geoPath().projection(projection);
 
   var mgmt_map = svg.append("g")
@@ -56,8 +56,7 @@ function init(){
     //yScale.domain([0, d3.max(data.values, function(d) { return d.h; })]);
     yScale.domain([0, 0.15]);
 
-    var bargraph = svg.append('g')
-      .attr("transform", "translate(0, 150)");
+    var bargraph = svg.append('g');
 
     bargraph.append("g")
       .attr("class", "axis axis-x")
